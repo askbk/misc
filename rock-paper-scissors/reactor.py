8c07d52dd694f11d2ccaaa219ddf6164d60bac26
ask <askbk@protonmail.com>
2018-04-02 23:55:49 +0200

    reactor finished.

--- rock-paper-scissors/reactor.py
+++ rock-paper-scissors/reactor.py
@@ -8,28 +8,38 @@ moves = ['r', 's', 'p']
 
 def main():
     global counter, moves
-
-    oppPoints, cPoints = 0, 0
+    oppPoints, cPoints, result = 0, 0, 0
+    oppMove = ""
 
     print("p1\tp2\t\tp1\tp2")
 
     while True:
 
-        oppMove = ""
-
-        if oppMove != "":
-            cMove = counter[oppMove]
-        else:
-            cMove = moves[randint(0, 2)]
+        cMove = nextMove(oppMove, result)
 
         oppMove = str(input())
 
         if counter[cMove] == oppMove:
             oppPoints += 1
+            result = -1
         elif counter[oppMove] == cMove:
             cPoints += 1
+            result = 1
+        else:
+            result = 0
 
         print(cMove, "\t", oppMove, "\t\t", cPoints, "\t", oppPoints)
+
+def nextMove(prevOpp, result):
+    if result != 0:
+
+        if result == 1:
+            return prevOpp
+        else:
+            return counter[prevOpp]
+
+    else:
+        return moves[randint(0, 2)]
 
 main()
 
